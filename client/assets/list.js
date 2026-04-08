ec6a34b715f59d44095f0cd704434498c84c1c3f
Tanjya Akther <tanjya26@gmail.com>
2026-04-08 15:10:00 +0100

    add delete, update button and style cards

--- client/assets/list.js
+++ client/assets/list.js
@@ -17,7 +17,7 @@ async function createMovieList(movieNames) {
 }
 
 
-function createMovieCard(data) {
+function createMovieCard(data, userRating = null) {
     // In this function we will create the elements of the card to be displayed
 
     //To define column and the div container for the card
@@ -48,20 +48,42 @@ function createMovieCard(data) {
 
     const yourRating = document.createElement("p");
     yourRating.classList.add("card-text");
-    yourRating.innerText = `You have not yet rated this movie`;
-    const button = document.createElement("button");
-    button.classList.add("btn", "btn-primary", "m-3");
-    button.type = "button";
-    button.innerText = "Update Rating";
-    yourRating.appendChild(button);
-
-
+    yourRating.innerText = userRating
+        ? `Your rating: ${userRating}/10`
+        : `You have not yet rated this movie`;
     cardBody.appendChild(yourRating);
+
+    const updateButton = document.createElement("button");
+    updateButton.classList.add("btn", "btn-primary", "m-3");
+    updateButton.type = "button";
+    updateButton.innerText = "Update Rating";
+
+    updateButton.addEventListener("click", () => {
+        const newRating = prompt("Enter your new rating (0-10):");
+        if (newRating !== null && !isNaN(newRating) && newRating >= 0 && newRating <= 10) {
+            yourRating.innerText = `Your rating: ${newRating}/10`;
+        } else {
+            alert("Please enter a valid rating between 0 and 10.");
+        }
+
+    });
+    cardBody.appendChild(updateButton);
 
     const year = document.createElement("p");
     year.classList.add("card-text");
     year.innerText = `This movie was released in ${data.Year}`;
     cardBody.appendChild(year);
+
+    //delete button
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-danger", "m-3");
+    deleteButton.type = "button";
+    deleteButton.innerText = "Delete";
+
+    deleteButton.addEventListener("click", () => {
+        column.remove();
+    });
 
     
     //append everything
@@ -70,6 +92,7 @@ function createMovieCard(data) {
     cardDiv.appendChild(cardBody);
     
     column.appendChild(cardDiv);
+    cardBody.appendChild(deleteButton);
 
     return column;
 }
@@ -90,11 +113,26 @@ async function getMovieInfo(name) {
 
 async function addMovieCardFromForm(e) {
     e.preventDefault();
-    const name = e.target.movieName.value;
-    e.target.movieName.value = "";
+    const name    = e.target.movieName.value;
+    const rating  = e.target.movieRating.value;
+    const user_id = localStorage.getItem('user_id');
+
+    e.target.movieName.value   = "";
+    e.target.movieRating.value = "";
+
+    await fetch('http://localhost:3000/rating', {
+        method:  'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body:    JSON.stringify({ movie_name: name, rating, user_id })
+    });
+
     const data = await getMovieInfo(name);
-    const card = createMovieCard(data);
+    const card = createMovieCard(data, rating);
     movieList.appendChild(card);
 }
+
+createMovieList();
+
+
 
 createMovieList(movies);
